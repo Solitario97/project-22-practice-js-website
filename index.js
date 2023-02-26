f0ad4f5ce17e2ea2ab2c4956553c14a56f14212c
Solitario97 <a.nurmagambetov97@gmail.com>
2023-02-26 08:06:50 +0600

    first commit v2

---
+++ index.js
@@ -0,0 +1,61 @@
+const site = document.querySelector('#site');
+
+const model = [
+    {type: 'title', value: 'Hello World from JS',},
+    {type: 'text', value: 'some text',},
+    {type: 'columns', value: [
+        '13',
+        '111',
+        '1211',
+        
+    ]}
+];
+
+model.forEach(block =>{
+    let html = '';
+    if (block.type === 'title') {
+        html = title(block);
+    } else if (block.type === 'text') {
+        html = text(block);
+    } else if (block.type === 'columns') {
+        html = columns(block);
+    } 
+
+    site.insertAdjacentHTML("beforeend", html);
+});
+
+function title (block) {
+    return `
+        <div class="row">
+            <div class="col-sm">
+                <h1>${block.value}</h1>
+            </div>
+        </div>
+    `
+}
+
+function text(block) {
+    return `
+        <div class="row">
+            <div class="col-sm">
+            <p>${block.value}</p>
+            </div>
+        </div>
+    `
+}
+
+function columns(block) {
+    let html = '';
+    block.value.forEach(item => {
+        html += `
+            <div class="col-sm">
+                ${item}
+            </div>
+        `
+    });
+    return `
+        <div class="row">
+           ${html} 
+        </div>
+    `
+}
